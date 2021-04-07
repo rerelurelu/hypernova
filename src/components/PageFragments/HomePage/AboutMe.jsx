c6bf559710e7a32710d3307ab286371a9ff724b0
snova <awaawapurin@gmail.com>
2021-04-08 03:32:31 +0900

    Change bio

--- src/components/PageFragments/HomePage/AboutMe.jsx
+++ src/components/PageFragments/HomePage/AboutMe.jsx
@@ -4,16 +4,11 @@ import { stripTags, domHtml } from '../../../utils/stripTags';
 import SEO from '../../Seo';
 
 const pageText = {
-  paraOne: `Hello !! My name is Rolwin Reevan Monteiro. I'm a full stack web developer who is
-    passionate about various web technologies. I like to experiment with different web
-    technologies. I have an experience of nearly 3 years working with LAMP stack, MERN stack
-    and ELK stack. Building fancy UI's just like this one that your seeing 😅 and writing blogs about tech stacks
-    is what Rolwin loves to do. Check my blog which I update every week for some Javascript and some
-    cool notes on web technologies.`,
-  paraTwo: `Currently I work mostly with Javascript technologies like ReactJS and NodeJS. I also
-    have hands on experience working with cloud infrastructures like <b>AWS/GCP</b> and have deployed applications
-    keeping scalability in mind. Docker, Kubernetes, Jenkins, SonarQube are some of the cool
-    tools I use for <b>CI/ CD</b>. I'm always a learner and a self taught programmer.`,
+  paraOne: `Hello, I'm  Zaw. I studied data science and machine learning at university.
+    At the university, I participated in a data analysis project with Shiga Lakestars,
+    and I was in charge of creating a program to detect players and balls.`,
+  paraTwo: `Currently I'm studying HTML/CSS, Javascript, and Django in order to acquire
+    knowledge in web developer skills.`,
 };
 
 const AboutMe = () => {
@@ -31,7 +26,9 @@ const AboutMe = () => {
         <p>
           {pageText.paraOne}
         </p>
-        <p dangerouslySetInnerHTML={domHtml(pageText.paraTwo)} />
+        <p>
+          {pageText.paraTwo}
+        </p>
       </div>
     </>
   );
