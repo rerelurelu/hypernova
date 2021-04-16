e77cf01cfd9f8e73c8f4f06e30fd80b80509acb9
snova <awaawapurin@gmail.com>
2021-04-17 03:26:42 +0900

    Initial commit

---
+++ src/components/PageFragments/ContactForm/index.jsx
@@ -0,0 +1,60 @@
+import {
+  Col, Form, Input, Button, message,
+} from 'antd';
+import React from 'react';
+
+import Config from '../../../../config';
+import style from './contactForm.module.less';
+
+const validateMessages = {
+  required: 'This field is required!',
+  types: {
+    email: 'Not a valid email!',
+  },
+};
+export default () => {
+  const [form] = Form.useForm();
+  const onFinish = (data) => {
+    const formData = new FormData();
+    // eslint-disable-next-line no-restricted-syntax
+    for (const key in data) {
+      if (data[key]) {
+        formData.append(key, data[key]);
+      }
+    }
+
+    fetch(Config.contactFormUrl, { method: 'POST', body: formData })
+      .then(() => {
+        message.success('Thank you for your kind response 🙂. Will get back to you.');
+        form.resetFields();
+      })
+      // eslint-disable-next-line no-console
+      .catch((error) => console.error('Error:', error));
+  };
+
+  return (
+    <>
+      <Col sm={24} md={24} lg={12} className="widthFull">
+        <Form form={form} name="nest-messages" onFinish={onFinish} validateMessages={validateMessages}>
+          <Form.Item name={['name']} rules={[{ required: true }]}>
+            <Input size="large" placeholder="Name *" />
+          </Form.Item>
+          <Form.Item name={['email']} rules={[{ type: 'email' }]}>
+            <Input size="large" placeholder="Email" />
+          </Form.Item>
+          <Form.Item name={['subject']} rules={[{ required: true }]}>
+            <Input size="large" placeholder="Subject *" />
+          </Form.Item>
+          <Form.Item name={['message']} rules={[{ required: true }]}>
+            <Input.TextArea size="large" rows={7} placeholder="Message *" />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" shape="round" size="large" htmlType="submit" style={{ background: '#304CFD' }}>
+              SEND
+            </Button>
+          </Form.Item>
+        </Form>
+      </Col>
+    </>
+  );
+};
---
+++ src/pages/contact/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Layout, Row, Col } from 'antd';
+
+import Header from '../../components/PageLayout/Header';
+import SidebarWrapper from '../../components/PageLayout/Sidebar';
+import ContactForm from '../../components/PageFragments/ContactForm';
+import SEO from '../../components/Seo';
+
+const Contact = () => (
+  <Layout className="outerPadding background">
+    <Layout className="container">
+      <SEO
+        title="Contact"
+        description=""
+        path="/contact"
+        keywords={['FullStack developer', 'Javascript', 'ReactJS', 'NodeJS', 'Gatsby', 'technology']}
+      />
+      <Header />
+      <SidebarWrapper>
+        <div className="marginTopTitle">
+          <h1 className="titleSeparate">Contact</h1>
+        </div>
+        <Row gutter={[40, 20]}>
+          <Col sm={24} md={24} lg={12}>
+            <div></div>
+          </Col>
+          <ContactForm />
+        </Row>
+      </SidebarWrapper>
+    </Layout>
+  </Layout>
+);
+
+export default Contact;
