811b2af3bd8b3f20493e3f949a795efb7c00b951
zoniha <awaawapurin@gmail.com>
2021-04-16 23:29:37 +0900

    Add Work link

--- src/components/PageLayout/Header/index.jsx
+++ src/components/PageLayout/Header/index.jsx
@@ -42,6 +42,11 @@ export default () => {
               </Link>
             </li>
             <li className={style.navItem}>
+              <Link to="/tags/work" onClick={toggleMenu} activeClassName={style.anchorActive}>
+                Work
+              </Link>
+            </li>
+            <li className={style.navItem}>
               <Link to="/blog" onClick={toggleMenu} activeClassName={style.anchorActive}>
                 Blog
               </Link>
