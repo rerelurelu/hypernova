a38cc9c927876bb611f759cc7cd7f2c1f6a40d42
snova <awaawapurin@gmail.com>
2021-04-24 03:19:29 +0900

    Add scrolling back to top

---
+++ src/components/Scroll/index.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import { faChevronUp } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import style from './scroll.module.less'
+
+const Scroll = ({ showBelow }) => {
+  const [show, setShow] = useState(showBelow ? false : true)
+
+  const handleScroll = () => {
+    if (window.pageYOffset > showBelow) {
+      if (!show) setShow(true)
+    } else {
+      if (show) setShow(false)
+    }
+  }
+
+  const handleClick = () => {
+    window[`scrollTo`]({ top: 0, behavior: `smooth` })
+  }
+
+  useEffect(() => {
+    if (showBelow) {
+      window.addEventListener(`scroll`, handleScroll)
+      return () => window.removeEventListener(`scroll`, handleScroll)
+    }
+  })
+
+  return (
+    <>
+      {show && (
+        <div onClick={handleClick} id={style.scrollBtn} aria-label="to top">
+          <FontAwesomeIcon icon={faChevronUp} className={style.arrowIcon} />
+        </div>
+      )}
+    </>
+  )
+}
+
+export default Scroll
