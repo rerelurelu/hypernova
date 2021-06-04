8923c906ba95873c2d17af17197a373e70761984
snova <awaawapurin@gmail.com>
2021-06-04 14:16:49 +0900

    ☪️ refactor: Renamed

---
+++ src/pages/error404/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '@fontsource/overpass';
+import '@fontsource/noto-sans-jp';
+
+import * as style from './error404.module.less';
+
+export default () => (
+  <div className={style.main}>
+    <div className={style.glitch} data-text="404">404</div>
+      <h3 className={`${style.outline} ${style.errorMessage}`}>page not found</h3>
+      <h3 className={style.errorMessage}>Perhaps you misspelled the url ot it has been removed.</h3>
+  </div>
+);
