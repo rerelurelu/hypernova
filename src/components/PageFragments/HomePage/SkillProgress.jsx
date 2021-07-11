d0aee275690d84a7750ccd5bdc81ed2508b908f1
zoniha <awaawapurin@gmail.com>
2021-07-12 00:27:20 +0900

    ✨ feat: Update contents

--- src/components/PageFragments/HomePage/SkillProgress.jsx
+++ src/components/PageFragments/HomePage/SkillProgress.jsx
@@ -8,20 +8,20 @@ const SkillsProgress = () => (
     <h2 className="titleSeparate">My Skills</h2>
     <Row gutter={[20, 20]}>
       <Col xs={24} sm={24} md={12}>
-        <ProgressBar percent={80} text="Python" />
-        <ProgressBar percent={55} text="Ruby" />
-        <ProgressBar percent={75} text="HTML / CSS" />
-        <ProgressBar percent={70} text="Django" />
-        <ProgressBar percent={65} text="Gatsby" />
-        <ProgressBar percent={65} text="PostgreSQL" />
+        <ProgressBar percent={70} text="Python" />
+        <ProgressBar percent={70} text="HTML / CSS" />
+        <ProgressBar percent={70} text="Sass" />
+        <ProgressBar percent={60} text="Gatsby" />
+        <ProgressBar percent={55} text="Flutter" />
+        <ProgressBar percent={65} text="Tailwind CSS" />
       </Col>
       <Col xs={24} sm={24} md={12}>
-        <ProgressBar percent={70} text="Javascript" />
-        <ProgressBar percent={65} text="Typescript" />
-        <ProgressBar percent={70} text="Sass" />
-        <ProgressBar percent={65} text="React" />
+        <ProgressBar percent={65} text="Javascript" />
+        <ProgressBar percent={55} text="Typescript" />
+        <ProgressBar percent={65} text="Django" />
+        <ProgressBar percent={60} text="React" />
         <ProgressBar percent={70} text="TensorFlow" />
-        <ProgressBar percent={70} text="Tailwind CSS" />
+        <ProgressBar percent={60} text="PostgreSQL" />
       </Col>
     </Row>
   </div>
